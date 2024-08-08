4734d4df3a054cb9c0b8580a36c7f1c9a679dcbc
Tomo Lazovich <lazovich@gmail.com>
2024-08-08 14:18:52 -0400

    more cleanup

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: ./proxy-server; ./pjc-server --host 0.0.0.0:10011 --input pjc_company.csv --stdout --no-tls
--- push-rpc-server.sh
+++ push-rpc-server.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-VARS_FILE=binary-buildpack-vars.yml
 PUSH_DIR=push_dir_rpc
 
 mkdir -p $PUSH_DIR
 cp ./target/x86_64-unknown-linux-gnu/release/pjc-server ./$PUSH_DIR
 cp manifest-rpc.yml ./$PUSH_DIR/manifest.yml
 cp Procfile-rpc ./$PUSH_DIR/Procfile
-cp $VARS_FILE ./$PUSH_DIR
 cp etc/example/pjc_company.csv ./$PUSH_DIR
 
 cd ./$PUSH_DIR
 
-cf push --vars-file $VARS_FILE test-lazovich-binary-pjc
+cf push test-lazovich-binary-pjc
 cf map-route test-lazovich-binary-pjc apps.internal --hostname test-lazovich-binary-pjc --app-protocol http2
